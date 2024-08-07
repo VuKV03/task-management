491f97dab475e5d7513235706243a805d9bdaf25
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-07 15:46:41 +0700

    build: change status

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -28,19 +28,15 @@ module.exports.index = async (req, res) => {
     limitItems: 2,
   };
   const countTasks = await Task.countDocuments(find);
-  const objPagination = paginationHelper(
-    initPagination,
-    req.query,
-    countTasks
-  );
+  const objPagination = paginationHelper(initPagination, req.query, countTasks);
   // End Pagination
 
   // Search
   let objSearch = searchHelper(req.query);
 
-  if(req.query.keyword) {
+  if (req.query.keyword) {
     find.title = objSearch.regex;
-  } 
+  }
   // End Search
 
   const tasks = await Task.find(find)
@@ -64,5 +60,29 @@ module.exports.detail = async (req, res) => {
     res.json(task);
   } catch (error) {
     res.json("Không tìm thấy!");
+  }
+};
+
+// [PATCH] /api/v1/tasks/change-status/:id
+module.exports.changeStatus = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const status = req.body.status;
+
+    await Task.updateOne({
+      _id: id
+    }, {
+      status: status
+    });
+
+    res.json({
+      code: 200,
+      message: "Cập nhật trạng thái thành công!"
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Không tồn tại!"
+    });
   }
 };
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 const env = require("dotenv")
 env.config();
 const database = require("./config/database");
@@ -8,6 +9,10 @@ const port = process.env.PORT;
 
 const routesApiV1 = require("./api/v1/routes/index.route");
 
+// parse application/json
+app.use(bodyParser.json());
+
+// Routes Version 1
 routesApiV1(app);
 
 app.listen(port, () => {
