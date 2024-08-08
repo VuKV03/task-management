692c1e29c3c7c2aa026e406906ca5bf0ceb04031
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-08 14:10:33 +0700

    build: cors, register

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require("express");
-const bodyParser = require("body-parser")
-const env = require("dotenv")
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const env = require("dotenv");
 env.config();
 const database = require("./config/database");
 database.connect();
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors());
+
 const routesApiV1 = require("./api/v1/routes/index.route");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser());
 
 // parse application/json
 app.use(bodyParser.json());
